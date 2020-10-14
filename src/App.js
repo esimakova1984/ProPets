b28981aba5776fd6803bbf6db6e9d812dc17ba8f
esimakova1984 <esimakova1984@gmail.com>
2020-10-14 20:08:00 +0300

    update ProPets project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StartPage from "./components/StartPage";
+import Hotels from "./components/Service/Hotels";
+import Fostering from "./components/Service/Fostering";
+import VetHelp from "./components/Service/VetHelp";
+import Walking from "./components/Service/Walking";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <StartPage/>
     </div>
   );
 }
